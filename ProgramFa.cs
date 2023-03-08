38d6a335f661f68a6c3c725104fbbaeedcce1f7a
António Pinto <antonio2219@gmail.com>
2023-03-08 16:48:41 +0000

    Facade

---
+++ ProgramFa.cs
@@ -0,0 +1,9 @@
+﻿using Singleton.Implementation;
+
+PaymentSingleton paymentSingleton = PaymentSingleton.GetInstance();
+TransferSingleton transferSingleton = TransferSingleton.GetInstance();
+
+Facade.Implementation.TransactionFacade transactionFacade = new Facade.Implementation.TransactionFacade(paymentSingleton, transferSingleton);
+
+transactionFacade.ExecutePayment();
+transactionFacade.ExecuteTransfer();
---
+++ TransactionFacade.cs
@@ -0,0 +1,28 @@
+﻿using Singleton.Implementation;
+
+namespace Facade.Implementation
+{
+    public class TransactionFacade
+    {
+        protected PaymentSingleton _paymentSingleton;
+        protected TransferSingleton _transferSingleton;
+
+        public TransactionFacade(PaymentSingleton paymentSingleton, TransferSingleton transferSingleton)
+        {
+            _paymentSingleton = paymentSingleton;
+            _transferSingleton = transferSingleton;
+        }
+
+        public void ExecutePayment()
+        {
+            _paymentSingleton.ExecutePayment();
+        }
+
+        public void ExecuteTransfer()
+        {
+            _transferSingleton.ExecuteTransfer();
+        }
+
+    }
+}
+
